db27f3596d46eb83756fc9b4e8de2ce443dfe9e3
shoshi3726 <112927363+shoshi3726@users.noreply.github.com>
2023-11-13 20:56:03 +0200

    Update weatherApp.js

--- weatherApp.js
+++ weatherApp.js
@@ -6,7 +6,6 @@ function init() {
 function doApi() {
   for (let i = 0; i < cities_arr.length; i++) {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cities_arr[i]}&units=metric&appid=21bfe76e4b5047875a7ee531ff8c0c17&lang=he`;
-    // let url = `https://api.openweathermap.org/data/2.5/weather?q=Jerusalem&units=metric&appid=21bfe76e4b5047875a7ee531ff8c0c17&lang=he`;
 
     $.get(url, function (resp) {
       console.log(resp);
